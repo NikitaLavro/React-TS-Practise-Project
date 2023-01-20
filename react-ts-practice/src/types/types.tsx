6e40a3d35b48e2d9a0f6b2f482aa149339f1a398
Nikita <lavro.nikita123@gmail.com>
2023-01-20 18:09:02 -0500

    UserList starting component

---
+++ react-ts-practice/src/types/types.tsx
@@ -0,0 +1,12 @@
+export interface IAddress {
+  street: string;
+  city: string;
+  zipCode: string;
+}
+
+export interface IUser {
+  id: number;
+  name: string;
+  email: string;
+  address: IAddress;
+}
