f788eda63b39dba7b0f2a53bb8ba2f5ea5d45e63
Nikita <lavro.nikita123@gmail.com>
2023-01-20 17:43:50 -0500

    Card Component and basic props interface

--- react-ts-practice/src/App.tsx
+++ react-ts-practice/src/App.tsx
@@ -1,7 +1,14 @@
 import React from "react";
 
+//Components
+import Card from "./components/Card";
+
 function App() {
-  return <div className="App">hi</div>;
+  return (
+    <div className="App">
+      <Card width="300px" height="50px" />
+    </div>
+  );
 }
 
 export default App;
