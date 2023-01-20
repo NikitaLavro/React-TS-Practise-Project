a0d61a741d2f04a2f2ced1de067ebf8ddbc37c20
Nikita <lavro.nikita123@gmail.com>
2023-01-20 18:17:46 -0500

    users prop and displaying users

--- react-ts-practice/src/App.tsx
+++ react-ts-practice/src/App.tsx
@@ -2,8 +2,23 @@ import React from "react";
 
 //Components
 import Card, { CardVariant } from "./components/Card";
+import UserList from "./components/UserList";
 
 function App() {
+  const users = [
+    {
+      id: 1,
+      name: "Nikita",
+      email: "asdasd@gmail.com",
+      address: { city: "Kharkiv", street: "123 street", zipCode: "12331" },
+    },
+    {
+      id: 2,
+      name: "Igor",
+      email: "vcxvxcv@gmail.com",
+      address: { city: "Kiev", street: "sfsdf street", zipCode: "12331" },
+    },
+  ];
   return (
     <div className="App">
       <Card
@@ -14,6 +29,7 @@ function App() {
       >
         <button>Click Me</button>
       </Card>
+      <UserList users={users} />
     </div>
   );
 }
--- react-ts-practice/src/components/UserList.tsx
+++ react-ts-practice/src/components/UserList.tsx
@@ -1,13 +1,21 @@
-import React,{FC} from 'react'
+import React, { FC } from "react";
+import { IUser } from "../types/types";
 
-interface UserListProps{
-    
+interface UserListProps {
+  users: IUser[];
 }
 
-const UserList: FC<UserListProps> = () => {
+const UserList: FC<UserListProps> = ({ users }) => {
   return (
-    <div>: FC<UserListProps></div>
-  )
-}
+    <div>
+      {users.map((user) => (
+        <div key={user.id} style={{ padding: 15, border: "1px solid gray" }}>
+          {user.id}. {user.name} lives in {user.address.city} on the
+          {user.address.street}
+        </div>
+      ))}
+    </div>
+  );
+};
 
-export default : FC<UserListProps>
+export default UserList;
