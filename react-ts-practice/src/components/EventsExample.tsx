3168baad20476c9e9adbd922f5fd3866b0f10f1e
Nikita <lavro.nikita123@gmail.com>
2023-01-20 21:11:17 -0500

    Drag&Drop implementation

--- react-ts-practice/src/components/EventsExample.tsx
+++ react-ts-practice/src/components/EventsExample.tsx
@@ -2,6 +2,7 @@ import React, { FC, useState } from "react";
 
 const EventsExample: FC = () => {
   const [value, setValue] = useState<string>("");
+  const [isDrag, setIsDrag] = useState<boolean>(false);
 
   const changeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
     setValue(e.target.value);
@@ -11,10 +12,46 @@ const EventsExample: FC = () => {
     console.log(value);
   };
 
+  const dragHandler = (e: React.DragEvent<HTMLDivElement>) => {
+    console.log("DRAG");
+  };
+
+  const dragWithPreventHandler = (e: React.DragEvent<HTMLDivElement>) => {
+    e.preventDefault();
+    setIsDrag(true);
+  };
+
+  const leaveHandler = (e: React.DragEvent<HTMLDivElement>) => {
+    e.preventDefault();
+    setIsDrag(false);
+  };
+
+  const dropHandler = (e: React.DragEvent<HTMLDivElement>) => {
+    e.preventDefault();
+    setIsDrag(true);
+    console.log("DROP");
+  };
+
   return (
     <div>
       <input type="text" onChange={changeHandler} />
       <button onClick={clickHandler}>Log input</button>
+      <div
+        draggable
+        onDrag={dragHandler}
+        style={{ width: 200, height: 200, background: "red", marginTop: 15 }}
+      />
+      <div
+        onDrop={dropHandler}
+        onDragLeave={leaveHandler}
+        onDragOver={dragWithPreventHandler}
+        style={{
+          width: 200,
+          height: 200,
+          background: isDrag ? "blue" : "red",
+          marginTop: 15,
+        }}
+      />
     </div>
   );
 };
